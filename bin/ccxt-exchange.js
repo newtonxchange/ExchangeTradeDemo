d506be9e9e9fa5d40fc12276af888be0ea4972d6
Scott Carr <scott@world-biobridge.com>
2019-05-08 10:34:51 +0800

    Made it so all exchanges are available for use without much boilerplate code.

--- bin/ccxt-exchange.js
+++ bin/ccxt-exchange.js
@@ -1,33 +1,26 @@
 let ccxt = require('ccxt');
 let newtonxchange = require('../bin/newtonxchange');
+const { Agent } = require('https')
 
-exports.getExchange = function(exchange, config) {
-    config.enableRateLimit = true;
-    switch (exchange) {
-        case "newton":
-            return new newtonxchange(config);
-        case "huobi":
-            return new ccxt.huobipro(config);
-        case "binance":
-            return new ccxt.binance(config);
-        case "bittrex":
-            return new ccxt.bittrex(config);
-        case "bitmex":
-            return new ccxt.bitmex(config);
-        case "negociecoins":
-            return new ccxt.negociecoins(config);
-        case "fcoin":
-            return new ccxt.fcoin(config);
-        case "okex":
-            return new ccxt.okex(config);
-        case "coinegg":
-            return new ccxt.coinegg(config);
-        case "cointiger":
-            return new ccxt.cointiger(config);
-        case "gateio":
-            return new ccxt.gateio(config);
-        case "zb":
-            return new ccxt.zb(config);
+exports.getExchange = function(exchangeId, config) {
+    if (exchangeId === "newton") {
+        return new newtonxchange(config);
     }
-    throw new Error("Unsupported exchange: " + exchange);
+    const verbose = config.verbose || false;
+    const debug = config.debug || false;
+    let enableRateLimit = config.enableRateLimit || true;
+    const agent = new Agent ({
+        ecdhCurve: 'auto',
+    });
+    try {
+        return new (ccxt)[exchangeId]({
+            agent,
+            verbose,
+            enableRateLimit,
+            debug,
+            timeout: 20000,
+        });
+    } catch (e) {
+        throw new Error("Unsupported exchange: " + exchangeId);
+    }
 };
